dd020da2cab5264332221e3d2e517ae8636a897d
mateuso12 <mateuso12@hotmail.com.br>
2023-03-02 22:19:42 -0300

    created product styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,47 @@
-import { styled } from '@/styles'
+import { HomeContainer, Product } from '@/styles/pages/home'
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$primary',
-  color: '$gray100',
-  borderRadius: '10px',
-  border: 'none',
-  padding: '10px',
-  fontWeight: '$medium',
-  fontSize: '$md',
-  lineHeight: 1,
-})
+import camiseta1 from '../assets/camisetas/1.svg'
+import camiseta2 from '../assets/camisetas/2.svg'
+import camiseta3 from '../assets/camisetas/3.svg'
 
 export default function Home() {
   return (
     <>
-      <Button>Hello World</Button>
+      <HomeContainer>
+        <Product>
+          <Image src={camiseta1} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta 1</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+        <Product>
+          <Image src={camiseta1} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta 1</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+        <Product>
+          <Image src={camiseta2} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta 1</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+        <Product>
+          <Image src={camiseta3} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta 1</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+      </HomeContainer>
     </>
   )
 }
